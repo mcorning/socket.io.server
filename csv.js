5da899a1971df82cf735baabae3daada31ed448e
Michael Corning <michael@secours.io>
2021-01-26 18:27:19 -0800

    Sisters data driven redisgraph visitors and rooms

--- csv.js
+++ csv.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-const path = require('path');
 const csv = require('fast-csv');
 
 const RedisGraph = require('redisgraph.js').Graph;
@@ -10,30 +9,59 @@ const options = {
 };
 
 const graph = new RedisGraph('sisters', null, null, options);
-const promises = [];
-fs.createReadStream(
-  path.resolve(__dirname, './', 'Sisters Business License Filtered Listing.csv')
-)
-  .pipe(csv.parse({ headers: true }))
-  .on('error', (error) => console.error(error))
-  .on('data', (row) => {
-    if (row.NAME === 'LODG') {
-      let x = `CREATE (:room{name:'${row.ID}',id:${row.CODE}})`;
-      console.log(x);
-      promises.push(graph.query(x));
-    }
-  })
-  .on('end', (rowCount) => {
-    console.log(`Parsed ${rowCount} rows`);
-    Promise.all(promises).then(() => {
-      let param = { id: 95 };
-      graph.query('MATCH (a {id: $id}) return a.name', param).then((res) => {
-        console.group('Print name of id 95:');
-        while (res.hasNext()) {
-          let record = res.next();
-          console.log(record.get('a.name'));
+const filter = 'LODG';
+if (filter) {
+  const promises = [];
+  let id;
+  fs.createReadStream('./Sisters Business License Filtered Listing.csv')
+    .pipe(csv.parse({ headers: true }))
+    .once('data', () => {
+      console.group('Rooms:');
+    })
+    .on('error', (error) => console.error(error))
+    .on('data', (row) => {
+      if (row.NAME === filter) {
+        let x = `CREATE (:room{name:"${row.ID}",id:${row.CODE}})`;
+        console.log(x);
+        promises.push(graph.query(x));
+        if (promises.length == 7) {
+          id = row.CODE;
         }
-        console.groupEnd();
+      }
+    })
+    .on('end', (rowCount) => {
+      console.log(`Parsed ${rowCount} rows`);
+      console.log(`Adding ${promises.length} nodes to graph`);
+      Promise.all(promises).then(() => {
+        graph.query(`MATCH (a {id: ${id}}) return a.name`).then((res) => {
+          console.group(`Print name of id ${id}:`);
+          while (res.hasNext()) {
+            let record = res.next();
+            console.log(record.get('a.name'));
+          }
+          console.groupEnd();
+        });
       });
+      console.groupEnd();
     });
+}
+
+// visitors
+console.log(' ');
+console.group('Visitors:');
+const visitors = require('./visitors.json');
+const allVisitors = [];
+visitors.forEach((visitor) => {
+  let x = `CREATE (:visitor{name:"${visitor.name}",id:${visitor.id}})`;
+  console.log(x);
+  allVisitors.push(graph.query(x));
+});
+Promise.all(allVisitors).then(() => {
+  graph.query(`MATCH (v:visitor) RETURN v.name`).then((res) => {
+    while (res.hasNext()) {
+      let record = res.next();
+      console.log(record.get('v.name'));
+    }
+    console.groupEnd();
   });
+});
