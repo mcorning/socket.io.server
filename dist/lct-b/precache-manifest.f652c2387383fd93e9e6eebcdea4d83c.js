9890618d8daf0d6e83b73cc400776f126a2ee3f0
Michael Corning <michael@secours.io>
2021-02-15 11:13:32 -0800

    lct-b specific build

---
+++ dist/lct-b/precache-manifest.f652c2387383fd93e9e6eebcdea4d83c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f3be2ce93ce1a7d36e",
+    "url": "/css/app.25db02ea.css"
+  },
+  {
+    "revision": "d94d91dfe1cec0f0e5c7",
+    "url": "/css/chunk-vendors.0093aee1.css"
+  },
+  {
+    "revision": "1ef4fcbf0d09597db908342b31e7fa70",
+    "url": "/data/sistersBusiness.json"
+  },
+  {
+    "revision": "c4a83868c08a5577f6ea14f46b7884d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39f3be2ce93ce1a7d36e",
+    "url": "/js/app.d2acf58e.js"
+  },
+  {
+    "revision": "d94d91dfe1cec0f0e5c7",
+    "url": "/js/chunk-vendors.f3f7fa7a.js"
+  },
+  {
+    "revision": "850b852ade58bcb6bd4f7f67c94b9f50",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
